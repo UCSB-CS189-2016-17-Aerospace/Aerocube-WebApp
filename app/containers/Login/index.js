979d2ecbf4f656202682d94b66eac0bdbe983f9f
Angel Ortega <an22ortega@gmail.com>
2016-11-02 01:24:43 -0700

    button

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-
+import styles from './style.css';
 
 export default class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+  }
 
-  render() {
+  render(){
     return (
       <div id="dialog-register" title="Register new user">
         <form>
           <fieldset>
             <div id="login-button">
-              <span id="login">Login</span><span id="register">Register</span>
+              <div className={styles.login}><span >Login></span></div><div className={styles.register}><span>Register</span></div>
             </div>
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" />
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
           </fieldset>
         </form>
       </div>
