5d746d757a4cb0db243d8d552245273d82d378bf
Angel Ortega <an22ortega@gmail.com>
2016-11-12 21:45:06 -0800

    test for props.email and props.password update in the component

--- app/containers/LoginPage/tests/index.test.js
+++ app/containers/LoginPage/tests/index.test.js
@@ -1,11 +1,28 @@
-// import LoginPage from '../index';
 
+import configureStore from '../../../store';
 import expect from 'expect';
 // import { shallow } from 'enzyme';
-// import React from 'react';
+import LoginPage from '../index';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-addons-test-utils';
+
+const store = configureStore();
 
 describe('<LoginPage />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('Both prop email and password are updated at submit click event', () => {
+    // Render LoginPage component with email and password props
+    const component = ReactTestUtils.renderIntoDocument(
+      <LoginPage store={store} email={'yourfirenation@gmail.com'} password={'helloworld'} />
+    );
+
+    // Retrieve button from LoginPage Component
+    const node = ReactTestUtils.findRenderedDOMComponentWithTag(component, 'button');
+
+    // Trigger Click event
+    ReactTestUtils.Simulate.click(node);
+    expect(node.type).toBe('submit');
+    expect(component.props.email).toEqual('yourfirenation@gmail.com');
+    expect(component.props.password).toEqual('helloworld');
   });
 });
