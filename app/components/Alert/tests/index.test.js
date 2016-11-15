6c8e23703fc04d2752a25d15e5646a3cf805be87
Eric Swenson <elswenson@umail.ucsb.edu>
2016-11-15 10:32:37 -0800

    Added unit tests, updated testing webpack configuration to allow testing CSS ClassNames

--- app/components/Alert/tests/index.test.js
+++ app/components/Alert/tests/index.test.js
@@ -3,18 +3,81 @@ import Alert from '../index';
 import expect from 'expect';
 import { shallow, mount } from 'enzyme';
 import React from 'react';
+import sinon from 'sinon';
 
 import styles from '../styles.css';
 
+let defaultHeader = 'This is a header';
+
+let defaultMessage = 'This is a message';
+
+let defaultShow = true;
+
+let defaultType = Alert.getInfoAlertType();
+
+
 let buildDefault = () => {
-  return <Alert header={'This is a header'} message={'This is a message'} show={true} type={Alert.getInfoAlertType()} />
+  return (
+    <Alert header={defaultHeader}
+           message={defaultMessage}
+           show={defaultShow}
+           type={defaultType}
+    />
+  )
 };
 
 describe('<Alert />', () => {
+  /** Render **/
   it('Renders to a div at the outer level', () => {
     const wrapper = shallow(buildDefault());
     expect(wrapper.type()).toEqual('div');
   });
+
+  it('Renders a h2 as the first element inside the outer div', () => {
+    const wrapper = shallow(buildDefault());
+    expect(wrapper.children().first().type()).toEqual('h2');
+  });
+
+
+  /** Negative Render **/
+
+  it('Renders only one h2 element', () => {
+    const wrapper = shallow(buildDefault());
+    expect(wrapper.find('h2').length).toEqual(1);
+  });
+
+  /** Default Props **/
+
+  it('Has the proper alert className on the outer div', () => {
+    const wrapper = shallow(buildDefault());
+    expect(wrapper.hasClass(styles.alert)).toEqual(true);
+  });
+
+  it('Has the proper alert type className on the outer div', () => {
+    const wrapper = shallow(buildDefault());
+    expect(wrapper.hasClass(Alert.getInfoAlertType())).toEqual(true);
+  });
+
+  it('Does not the blockAlert className on the outer div', () => {
+    const wrapper = shallow(buildDefault());
+    expect(wrapper.hasClass(styles.blockAlert)).toEqual(Alert.defaultProps.block);
+  });
+
+  it('Has the proper className on the header', () => {
+    const wrapper = shallow(buildDefault());
+    expect(wrapper.find('h2').hasClass(styles.alertHeader)).toEqual(true);
+  });
+
+  it('Has the proper className on the message', () => {
+    const wrapper = shallow(buildDefault());
+    expect(wrapper.find('p').hasClass(styles.alertMessage)).toEqual(true);
+  });
+
+  it('Does not render a \'hide\' button', () => {
+    const wrapper = shallow(buildDefault());
+    expect(wrapper.find('button').length).toEqual(0)
+  });
+
 
   /** Props **/
 
@@ -41,11 +104,11 @@ describe('<Alert />', () => {
   });
 
   it('Correctly returns the warningAlert className', () => {
-    expect(Alert.getWarningAlertType()).toEqual(styles.errorAlert);
+    expect(Alert.getWarningAlertType()).toEqual(styles.warningAlert);
   });
 
   it('Correctly returns the infoAlert className', () => {
-    expect(Alert.getInfoAlertType()).toEqual(styles.errorAlert);
+    expect(Alert.getInfoAlertType()).toEqual(styles.infoAlert);
   });
 });
 
