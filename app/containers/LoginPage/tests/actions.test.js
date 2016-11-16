45e3a3de2d8d7df080f84c8e38e7db1e76dd0255
Eric Swenson <elswenson@umail.ucsb.edu>
2016-11-15 23:39:41 -0800

    Added unit tests for actions and reducers

--- app/containers/LoginPage/tests/actions.test.js
+++ app/containers/LoginPage/tests/actions.test.js
@@ -1,18 +1,36 @@
 import expect from 'expect';
-import {
-  defaultAction,
-} from '../actions';
-import {
-  DEFAULT_ACTION,
-} from '../constants';
+import * as actions from '../actions';
+import * as constants from '../constants';
 
 describe('LoginPage actions', () => {
   describe('Default Action', () => {
     it('has a type of DEFAULT_ACTION', () => {
       const expected = {
-        type: DEFAULT_ACTION,
+        type: constants.DEFAULT_ACTION,
       };
-      expect(defaultAction()).toEqual(expected);
+      expect(actions.defaultAction()).toEqual(expected);
     });
   });
+
+  describe('UpdateEmail Action', () => {
+    it('has a type of UPDATE_EMAIL', () => {
+      expect(actions.updateEmail('').type).toEqual(constants.UPDATE_EMAIL);
+    });
+
+    it('passes an unchanged email parameter on to the created action', () => {
+      const newTestEmail = 'thisTestEmail';
+      expect(actions.updateEmail(newTestEmail).email).toEqual(newTestEmail)
+    });
+  });
+
+  describe('UpdatePassword Action', () => {
+    it('has a type of UPDATE_PASSWORD', () => {
+      expect(actions.updatePassword('').type).toEqual(constants.UPDATE_PASSWORD);
+    });
+
+    it('passes an unchanged email parameter on to the created action', () => {
+      const newTestPassword = 'badPasswordForTesting';
+      expect(actions.updateEmail(newTestPassword).email).toEqual(newTestPassword)
+    });
+  })
 });
