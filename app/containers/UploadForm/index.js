9e567f900922e500885623ba886e1ef9dec39b30
Eric Swenson <elswenson@umail.ucsb.edu>
2016-11-27 02:11:22 -0800

    Switched to use Alert component

--- app/containers/UploadForm/index.js
+++ app/containers/UploadForm/index.js
@@ -15,6 +15,7 @@ const scroll = Scroll.scroller;
 import selectUploadForm from './selectors';
 
 import Img from '../../components/Img/Img';
+import Alert from '../../components/Alert/index';
 import APIClient from '../../utils/apiUtils';
 
 import styles from './styles.css';
@@ -23,6 +24,10 @@ export class UploadForm extends React.Component { // eslint-disable-line react/p
   constructor(props) {
     super(props);
     this.state = {
+      showAlert: false,
+      alertHeader: '',
+      alertMessage: '',
+      alertType: Alert.getInfoAlertType(),
       processingImage: false,
       files: [],
       img: undefined,
@@ -34,35 +39,42 @@ export class UploadForm extends React.Component { // eslint-disable-line react/p
   onDrop = (acceptedFiles) => {
     let self = this;
     self.setState({
-      processingImage: true
-    }, () => {
-      // Set image to be "processing". Note onDrop isn't called by the DropZone until
-      // much of the processing has already been done.
-      if(acceptedFiles && acceptedFiles.length == 1) {
-        let reader = new FileReader();
-        reader.onloadend = () => {
-          // Called once the img is loaded in for preview by the FileReader
-          self.setState({
-            files: acceptedFiles,
-            img: acceptedFiles[0],
-            imgPreviewUrl: reader.result,
-            processingImage: false
-          }, () => { // Scroll to location once processed
-            scroll.scrollTo('UploadImg', {
-              duration: 600,
-              delay: 200,
-              smooth: true,
-              offset: -75,
-              isDynamic: true
-            });
-          })
-        };
-        // Starts the process of reading the file
-        reader.readAsDataURL(acceptedFiles[0]);
-      } else {
-        // TODO: Handle error case (Should only occur if file rejected, due to DropZone prop multiple=false)
-      }
+      processingImage: true,
+      alertHeader: 'Please Wait',
+      alertMessage: 'Your image is being processed',
+      alertType: Alert.getInfoAlertType(),
+      showAlert: true
     });
+    // Set image to be "processing". Note onDrop isn't called by the DropZone until
+    // much of the processing has already been done.
+    if(acceptedFiles && acceptedFiles.length == 1) {
+      let reader = new FileReader();
+      reader.onloadend = () => {
+        // Called once the img is loaded in for preview by the FileReader
+        self.setState({
+          files: acceptedFiles,
+          img: acceptedFiles[0],
+          imgPreviewUrl: reader.result,
+          processingImage: false,
+          alertHeader: 'Image Processed',
+          alertMessage: 'Please check the preview to make sure you selected the correct image',
+          alertType: Alert.getSuccessAlertType(),
+          showAlert: true
+        }, () => { // Scroll to location once processed
+          scroll.scrollTo('UploadImg', {
+            duration: 600,
+            delay: 200,
+            smooth: true,
+            offset: -75,
+            isDynamic: true
+          });
+        })
+      };
+      // Starts the process of reading the file
+      reader.readAsDataURL(acceptedFiles[0]);
+    } else {
+      // TODO: Handle error case (Should only occur if file rejected, due to DropZone prop multiple=false)
+    }
   };
 
   clearImage = (evt) => {
@@ -72,22 +84,55 @@ export class UploadForm extends React.Component { // eslint-disable-line react/p
     this.setState({
       files: [],
       img: undefined,
-      imgPreviewUrl: undefined
+      imgPreviewUrl: undefined,
+      alertHeader: 'Cleared',
+      alertMessage: 'You may now select another image',
+      alertType: Alert.getInfoAlertType(),
+      showAlert: true
     });
   };
 
   handleUpload = (evt) => {
+    let self = this;
     if(evt && evt.preventDefault) {
       evt.preventDefault();
     }
-    let data = new FormData();
-    data.append('photo', this.state.img);
-    let responsePromise = APIClient.post('uploadImage', data);
-    responsePromise.then((response) => {
-      console.log(response.data);
-      alert(response.data['upload status'])
-    }, (err) => {
-      alert(err.message);
+    self.setState({
+      alertHeader: 'Please Wait',
+      alertMessage: 'Your image is being uploaded',
+      alertType: Alert.getInfoAlertType(),
+      showAlert: true
+    }, () => {
+      let data = new FormData();
+      data.append('photo', self.state.img);
+      APIClient.post('uploadImage', data).then((response) => {
+        console.log(response.data);
+        self.setState({
+          alertHeader: 'Success',
+          alertMessage: response.data['upload status'],
+          alertType: Alert.getSuccessAlertType(),
+          showAlert: true
+        }, () => scroll.scrollTo('UploadAlert', {
+          duration: 600,
+          delay: 200,
+          smooth: true,
+          isDynamic: true,
+          offset: -50
+        }));
+      }, (err) => {
+        self.setState({
+          alertHeader: 'Error',
+          alertMessage: err.message,
+          alertType: Alert.getErrorAlertType(),
+          showAlert: true
+        }, () => scroll.scrollTo('UploadAlert', {
+          duration: 600,
+          delay: 200,
+          smooth: true,
+          isDynamic: true,
+          offset: -50
+        }));
+      });
     });
   };
 
@@ -100,7 +145,7 @@ export class UploadForm extends React.Component { // eslint-disable-line react/p
              lazyLoad={true}
              spinnerWrapperStyle={{width: '100%', height: 300}}
              src={self.state.imgPreviewUrl} />
-        <button type="submit" className={styles.submitButton} onClick={(evt) => self.handleUpload(evt)}>
+        <button type="submit" className={[styles.button, styles.submitButton].join(' ')} onClick={(evt) => self.handleUpload(evt)}>
           Upload Image
         </button>
         <p className={styles.selectAnotherText}>
@@ -131,6 +176,12 @@ export class UploadForm extends React.Component { // eslint-disable-line react/p
             { name: 'description', content: 'Description of UploadForm' },
           ]}
         />
+        <Element name="UploadAlert" />
+        <Alert show={self.state.showAlert}
+               header={self.state.alertHeader}
+               message={self.state.alertMessage}
+               type={self.state.alertType}
+               showHideButton={true} />
         <form onSubmit={(evt) => self.handleUpload(evt)} className={styles.uploadForm}>
           <h1 className={styles.uploadFormHeader}>
             Upload an Image
